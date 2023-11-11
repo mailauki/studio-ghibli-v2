b0e78106baff47a62d8239bdb60a4c261bcf358c
Julie Evans <juliemevans17@gmail.com>
2023-11-10 20:43:33 -0600

    Add filter

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,13 @@ import { styled } from '@mui/material/styles'
 import CardContainer from './components/container'
 import CardDetail from './components/detail'
 
-import { AppBar, Box, Container, CssBaseline, Drawer, Toolbar, Typography, useMediaQuery } from '@mui/material'
+import { AppBar, Box, Collapse, Container, CssBaseline, Drawer, FormControl, InputLabel, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ListSubheader, MenuItem, Select, Toolbar, Typography, useMediaQuery } from '@mui/material'
 
 import { DetailContext, Film } from './utils/film'
+import { ExpandLess, ExpandMore, Inbox, StarBorder } from '@mui/icons-material'
 
-// const drawerWidth = '50%'
-const drawerWidth = 600
+// const drawerWidth = 600
+const drawerWidth = '50vw'
 
 const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
   open?: boolean;
@@ -23,8 +24,8 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen
   }),
-  // marginRight: '-50%',
-  marginRight: -drawerWidth,
+  // marginRight: -drawerWidth,
+  marginRight: '-50vw',
   ...(open && {
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.easeOut,
@@ -49,6 +50,16 @@ export default function Home() {
     setOpen(false)
   }
 
+  const [filter, setFilter] = useState(undefined)
+  const [filterOpen, setFilterOpen] = useState(true)
+
+  function handleFilterChange() {
+  }
+
+  const handleClick = () => {
+    setFilterOpen(!filterOpen)
+  }
+
   return (
     <DetailContext.Provider value={{ detail, setDetail, handleDrawerOpen, handleDrawerClose }}>
       <CssBaseline />
@@ -65,6 +76,26 @@ export default function Home() {
           {matches ? (
             <Main open={open}>
               <Toolbar />
+              <FormControl fullWidth sx={{ mb: 2 }}>
+                <InputLabel id="demo-simple-select-label">Filter by</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={filter}
+                  label='Filter by'
+                  onChange={handleFilterChange}
+                >
+                  <MenuItem value=''><em>None</em></MenuItem>
+                  <MenuItem value='watched'>Watched</MenuItem>
+                  <MenuItem value='not-watched'>Not Watched</MenuItem>
+                    <ListSubheader>Director</ListSubheader>
+                    <MenuItem sx={{ pl: 4 }} value='hayao-miyazaki'>Hayao Miyazaki</MenuItem>
+                    <MenuItem sx={{ pl: 4 }} value='isao-takahata'>Isao Takahata</MenuItem>
+                    <MenuItem sx={{ pl: 4 }} value='yoshifumi-kondo'>Yoshifumi Kondō</MenuItem>
+                    <MenuItem sx={{ pl: 4 }} value='hiroyuki-morita'>Hiroyuki Morita</MenuItem>
+                    <MenuItem sx={{ pl: 4 }} value='goro-miyazaki'>Gorō Miyazaki</MenuItem>
+                </Select>
+              </FormControl>
               <CardContainer open={open} />
             </Main>
           ) : (
